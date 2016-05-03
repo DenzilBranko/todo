57542baeeb340a11b5aabd02ec17a20f038aea0c
denzil branko <denzilbranko@gmail.com>
2016-05-03 06:52:49 -0400

    user can add new todo item

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 define([
-"./config"
-], function(config){
-    require(["myapp"])
+    "./config"
+], function(config) {
+
+    require(["views/list"], function(ListView) {
+
+        list = new ListView();
+        $("#content").append(list.$el);
+
+        list.collection.add([{
+            name: "Clean the house"
+        }]);
+
+        list.collection.add([{
+            name: "Oil hair"
+        }]);
+
+        list.collection.add([{
+            name: "Print certificate"
+        }]);
+
+        $("button").on("click",function(){
+           var  val = $("input").val();
+            if (val){
+               list.collection.add([{
+                   name : val
+               }]);
+            }
+        });
+        
+    });
+
 });
--- js/views/list.js
+++ js/views/list.js
@@ -1,5 +1,30 @@
 define([
-"backbone"
-], function(){
-    window.alert("welcome to myapp");
+    "../collections/todo"
+], function(TodoCollection) {
+    
+    return Backbone.View.extend({
+
+      tagName: "ul",
+
+      className: "todo-items",
+
+      events: {
+
+      },
+
+      initialize: function() {
+        this.collection = new TodoCollection();
+        this.collection.on("add", this.addToList, this);
+      },
+
+      addToList: function(model) {
+        this.$el.append("<li>" + model.get("name") + "</li>");
+      },
+      
+      render: function() {
+            
+      }
+
+    });
+
 });
